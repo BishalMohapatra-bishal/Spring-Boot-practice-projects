e56f32c7eed975f28db6f78731ebe1e914fcdb4f
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-16 22:31:03 +0530

    controller class

--- src/main/java/com/Personal/Bookmark/Personal/Bookmark/controller/NoteController.java
+++ src/main/java/com/Personal/Bookmark/Personal/Bookmark/controller/NoteController.java
@@ -1,7 +1,46 @@
 package com.Personal.Bookmark.Personal.Bookmark.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Personal.Bookmark.Personal.Bookmark.dto.NoteRequestDto;
+import com.Personal.Bookmark.Personal.Bookmark.dto.NoteResponseDto;
+import com.Personal.Bookmark.Personal.Bookmark.service.NoteService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class NoteController {
+
+    private final NoteService noteService;
+
+    @PostMapping
+    public ResponseEntity<NoteResponseDto> createNote( @Valid @RequestBody NoteRequestDto noteRequestDto) {
+        NoteResponseDto responseDto = noteService.createNote(noteRequestDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NoteResponseDto>> getNotes(
+            @RequestParam(required = false) String tag,
+            @RequestParam(required = false) String category) {
+        return ResponseEntity.ok(noteService.getNotes(tag, category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NoteResponseDto> getNoteById(@PathVariable Long id) {
+        return ResponseEntity.ok(noteService.getNoteById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NoteResponseDto> updateNote(
+            @PathVariable Long id,
+            @Valid @RequestBody NoteRequestDto requestDto ) {
+        return ResponseEntity.ok(noteService.updateNote(id, requestDto));
+    }
+
+
 }
