cf7c4983307da3e89ac8f40eb686896097abf889
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-26 23:16:38 +0530

    -Create the cart item entity with jpa mapping

--- GlobalExceptionDemo/src/main/java/com/GlobalExceptionDemo/GlobalExceptionDemo/model/Cart_item.java
+++ GlobalExceptionDemo/src/main/java/com/GlobalExceptionDemo/GlobalExceptionDemo/model/Cart_item.java
@@ -1,4 +1,30 @@
 package com.GlobalExceptionDemo.GlobalExceptionDemo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Cart_item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_Id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
 }
