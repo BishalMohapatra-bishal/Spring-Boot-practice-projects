072b551564f6655dd0ae22c2ce3a3b49fab5bffa
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-26 23:17:35 +0530

    -Create the seller  entity with jpa mapping

--- GlobalExceptionDemo/src/main/java/com/GlobalExceptionDemo/GlobalExceptionDemo/model/Seller.java
+++ GlobalExceptionDemo/src/main/java/com/GlobalExceptionDemo/GlobalExceptionDemo/model/Seller.java
@@ -1,4 +1,29 @@
 package com.GlobalExceptionDemo.GlobalExceptionDemo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String productName;
+
+//    @OneToMany
+//
+//    private Product product;
 }
